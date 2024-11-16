180c14aced0b1713e7901510ae89f2bdc2213c38
AnatolyDolgih <tolick.dolgih2013@yandex.ru>
2024-11-16 13:26:45 +0300

    Add class for VT

--- src/baseMoralScheme.py
+++ src/baseMoralScheme.py
@@ -1,5 +1,9 @@
 import numpy as np
 
 class BaseMoralScheme():
-    def __init__(self):
+    def __init__(self, init_state):
         print(f"moral scheme ctor")
+        self.cur_state = init_state
+    
+    def update_state(self, cur_impact):
+        self.cur_state += cur_impact
---
+++ src/virtualTutor.py
@@ -0,0 +1,8 @@
+import baseMoralScheme as bsm
+
+class VirtualTutor():
+    def __init__(self):
+        self.moral_1 = bsm.BaseMoralScheme()
+        self.moral_2 = bsm.BaseMoralScheme()
+        self.moral_3 = bsm.BaseMoralScheme()
+        self.moral_4 = bsm.BaseMoralScheme()
